ba7ce999b3aff13cfff98f4818c28b653747d86c
Timo Hausmann <hello@timohausmann.de>
2013-10-26 00:33:07 +0200

    grunt autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,15 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 
 	grunt.initConfig({
 
 		watch : {
 
 			scss : {
-				files: ['assets/css/src/*.scss'],
-				tasks: ['sass:scss']
+				files: ['assets/css/src/**/*.scss'],
+				tasks: ['sass:scss', 'autoprefixer:scss']
 			}
 		},
 
@@ -32,6 +33,17 @@ module.exports = function(grunt) {
 
 					]
 				}
+			}
+		},
+
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 version']
+			},
+
+			scss : {
+				src: 'assets/css/style.css',
+				dest: 'assets/css/style.css'
 			}
 		}
 		
